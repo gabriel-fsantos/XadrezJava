f6d6155c760453d58b2f7c96c5d7b514d9f1a1e0
Gabriel Ferreira dos Santos <biel0fds@gmail.com>
2020-01-05 13:18:58 -0300

    Posição

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,11 @@
+package aplicacao;
+
+import tabuleiro.Posicao;
+
+public class Programa {
+    public static void main (String[] args){
+    
+        Posicao p = new Posicao(1,2);
+        System.out.println(p);
+    }
+}
